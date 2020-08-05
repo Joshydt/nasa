3e3f1b30bd696a5575be3f90d1345bbe9dd5ed9a
Joshua Tyler <josht@userconversion.com>
2020-08-05 19:16:18 +0100

    built in the fetch function and parsed the json on submission

--- src/components/app.js
+++ src/components/app.js
@@ -4,25 +4,44 @@ import { Router } from 'preact-router';
 import Header from './header';
 import Home from './home';
 import Profile from './profile';
+import SearchForm from './form/index';
 
 export default class App extends Component {
-	/** Gets fired when the route changes.
-	 *	@param {Object} event		"change" event from [preact-router](http://git.io/preact-router)
-	 *	@param {string} event.url	The newly routed URL
-	 */
-	handleRoute = e => {
-		this.currentUrl = e.url;
-	};
+
+	constructor(props) {
+		super(props);
+		this.state = {
+		  results: {}
+		};
+	
+		this.fetchResults = this.fetchResults.bind(this);
+	}
+
+
+	// Fetch the query results
+	fetchResults = q => {
+		fetch(`https://images-api.nasa.gov/search?&media_type=image&q=${q}`).then((res) => {
+			if (res) {
+				return res.json()
+			}
+		}).then((jsonObj) => {
+			this.setState({
+				results: jsonObj
+			});
+			console.log('this state = ', this.state);
+		}).catch((err) => {
+			console.error('couldn\'t fetch results, ', err);
+			return;
+		})
+	}
+
 
 	render() {
 		return (
 			<div id="app">
-				<Header />
-				<Router onChange={this.handleRoute}>
-					<Home path="/" />
-					<Profile path="/profile/" user="me" />
-					<Profile path="/profile/:user" />
-				</Router>
+				{/* <Header /> */}
+				<h1>Home</h1>
+				<SearchForm func={this.fetchResults} />
 			</div>
 		);
 	}
